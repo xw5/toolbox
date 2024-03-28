128d0e1ebf632844c0fbe303dce0eb764097c391
xiewu <xiewu1@meizu.com>
2024-03-28 21:49:32 +0800

    fix(工具箱): 完善和优化工具箱

--- web/toolbox/src/pages/UrlEncode.vue
+++ web/toolbox/src/pages/UrlEncode.vue
@@ -149,9 +149,198 @@ const copy = (url) => {
       <li>encodeURIComponent 比 encodeURI 编码的范围大</li>
       <li>当你需要编码整个 URL，就用 encodeURI,只需要编码 URL 中的参数时，就使用 encodeURIComponent</li>
     </ul>
+    <div class="w-full flex flex-col mt-[10px]">
+      <p class="text-[16px] text-[#333] mb-[5px]">以下是常见符号的 URL 编码表格，包括符号、URL编码和描述：</p>
+      <table class="urlcode-table" cellspacing="0">
+        <thead class="table-secondary">
+          <tr>
+            <th>符号</th>
+            <th>URL编码</th>
+            <th>描述</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>空格</td>
+            <td>%20</td>
+            <td>空格</td>
+          </tr>
+          <tr>
+            <td>"</td>
+            <td>%22</td>
+            <td>双引号</td>
+          </tr>
+          <tr>
+            <td>#</td>
+            <td>%23</td>
+            <td>井号</td>
+          </tr>
+          <tr>
+            <td>$</td>
+            <td>%24</td>
+            <td>美元符号</td>
+          </tr>
+          <tr>
+            <td>%</td>
+            <td>%25</td>
+            <td>百分号</td>
+          </tr>
+          <tr>
+            <td>&amp;</td>
+            <td>%26</td>
+            <td>和号</td>
+          </tr>
+          <tr>
+            <td>'</td>
+            <td>%27</td>
+            <td>单引号</td>
+          </tr>
+          <tr>
+            <td>(</td>
+            <td>%28</td>
+            <td>左圆括号</td>
+          </tr>
+          <tr>
+            <td>)</td>
+            <td>%29</td>
+            <td>右圆括号</td>
+          </tr>
+          <tr>
+            <td>*</td>
+            <td>%2A</td>
+            <td>星号</td>
+          </tr>
+          <tr>
+            <td>+</td>
+            <td>%2B</td>
+            <td>加号</td>
+          </tr>
+          <tr>
+            <td>,</td>
+            <td>%2C</td>
+            <td>逗号</td>
+          </tr>
+          <tr>
+            <td>/</td>
+            <td>%2F</td>
+            <td>斜杠</td>
+          </tr>
+          <tr>
+            <td>:</td>
+            <td>%3A</td>
+            <td>冒号</td>
+          </tr>
+          <tr>
+            <td>;</td>
+            <td>%3B</td>
+            <td>分号</td>
+          </tr>
+          <tr>
+            <td>&lt;</td>
+            <td>%3C</td>
+            <td>小于号</td>
+          </tr>
+          <tr>
+            <td>=</td>
+            <td>%3D</td>
+            <td>等号</td>
+          </tr>
+          <tr>
+            <td>&gt;</td>
+            <td>%3E</td>
+            <td>大于号</td>
+          </tr>
+          <tr>
+            <td>?</td>
+            <td>%3F</td>
+            <td>问号</td>
+          </tr>
+          <tr>
+            <td>@</td>
+            <td>%40</td>
+            <td>亚瑟斯</td>
+          </tr>
+          <tr>
+            <td>[</td>
+            <td>%5B</td>
+            <td>左方括号</td>
+          </tr>
+          <tr>
+            <td>\</td>
+            <td>%5C</td>
+            <td>反斜杠</td>
+          </tr>
+          <tr>
+            <td>]</td>
+            <td>%5D</td>
+            <td>右方括号</td>
+          </tr>
+          <tr>
+            <td>^</td>
+            <td>%5E</td>
+            <td>尖角号</td>
+          </tr>
+          <tr>
+            <td>_</td>
+            <td>%5F</td>
+            <td>下划线</td>
+          </tr>
+          <tr>
+            <td>`</td>
+            <td>%60</td>
+            <td>反引号</td>
+          </tr>
+          <tr>
+            <td>{</td>
+            <td>%7B</td>
+            <td>左花括号</td>
+          </tr>
+          <tr>
+            <td>|</td>
+            <td>%7C</td>
+            <td>垂直线</td>
+          </tr>
+          <tr>
+            <td>}</td>
+            <td>%7D</td>
+            <td>右花括号</td>
+          </tr>
+          <tr>
+            <td>~</td>
+            <td>%7E</td>
+            <td>波浪号</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.urlcode-table{
+  width: 100%;
+  text-align: center;
+  thead{
+    tr{
+      border: 1px solid red;
+      background-color: #dfdfdd;
+      th{
+        height: 32px;
+      }
+    }
+  }
+  tbody{
+    tr{
+      td{
+        height: 32px;
+      }
+    }
+    tr:nth-child(2n){
+      background-color: #f2f2f2;
+    }
+    tr:nth-child(2n+1){
+      background-color: #ffffff;
+    }
+  }
+}
 </style>
